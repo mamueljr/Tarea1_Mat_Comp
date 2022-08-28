8a5c611822a02115f4c134880c436179dc9250a6
Adalberto Emmanuel Rojas <mamueljr@gmail.com>
2022-08-27 20:45:50 -0600

    Update tarea1MC.py

--- tarea1MC.py
+++ tarea1MC.py
@@ -9,20 +9,26 @@ import numpy as np
 
 # NUMERO DE PUNTOS
 radio = 1
-puntos = 100
+#puntos = 100
 #puntos = 1000
 #puntos = 10000
-#puntos = 1000000
+puntos = 1000000
 
 # METODO APLICADO
 pc = 0  # puntosdentro del circulo
 i = 0
-x = rand.random()*(2*radio)-1
-y = rand.random()*(2*radio)-1
+while i<puntos:
+    x = rand.random()*(2*radio)-1
+    y = rand.random()*(2*radio)-1
+    
 # calcular distancia del punto
-d = np.sqrt(x**2+y**2)
-if d <= radio:
-    pc = pc+1
+    d = np.sqrt(x**2+y**2)
+    if d <= radio:
+        pc = pc+1
+    i=i+1
+        
+valorpi= 4*pc/i       
 # RESULTADOS
-print(x, y)
-print(pc, puntos)
+#print(x, y)
+#print(pc, puntos)
+print('Valor aproximado de pi con 10 000 000 puntos:',valorpi)
