461b2d37ef6bb2ed922b4f8eab84f81b92815fb3
Adalberto Emmanuel Rojas <mamueljr@gmail.com>
2022-08-27 19:37:41 -0600

    Create tarea1MC.py

---
+++ tarea1MC.py
@@ -0,0 +1,22 @@
+# ADALBERTO EMMANUEL ROJAS
+# TAREA 1 MATEMATICAS COMPUTACIONALES
+# APROXIMACION A PI CON EL METODO MONTECARLO
+
+# Importamos libreria random
+import random as rand
+
+
+# NUMERO DE PUNTOS
+radio = 1
+puntos = 100
+#puntos = 1000
+#puntos = 10000
+#puntos = 1000000
+
+# METODO APLICADO
+i = 0
+x = rand.random()*(2*radio)-1
+y = rand.random()*(2*radio)-1
+
+# RESULTADOS
+print(x, y)
